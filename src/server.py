2cc4426e21f93c8c47a7601fe76b4c9aee4dd988
phamlam24 <ptlam61004@gmail.com>
2024-09-14 14:13:00 -0400

    finish backend 0.1

--- src/server.py
+++ src/server.py
@@ -1,11 +1,14 @@
 import os
+from pathlib import Path
 
 from flask import Flask
 from flask_cors import CORS
-from flask import request, send_file
+from flask import request, send_file, jsonify
 from services.OpenAI import OpenAIService
 
 from dotenv import load_dotenv 
+
+import json
 
 load_dotenv()
 
@@ -17,8 +20,8 @@ api_key = os.getenv('OPENAI_API_KEY')
 HTTP_OK = 200
 HTTP_BAD_REQUEST = 400
 
-VOICE = os.getenv('VOICE')
-MODEL = os.getenv('MODEL')
+VOICE = os.getenv('TTS_VOICE')
+MODEL = os.getenv('TTS_MODEL')
 ASSISTANT_ID = os.getenv('OPENAI_ASSISTANT_DISPATCHER_ID')
 
 openai = OpenAIService(os.getenv('OPENAI_API_KEY'))
@@ -31,17 +34,46 @@ def hello():
 def api():
     return 'Welcome to the API!'
 
+# Return the ID of a new chat thread
+@app.route('/api/new_chat', methods=['GET'])
+def new_chat():
+    id = openai.new_chat()
+    return id, HTTP_OK
+
+# Return the audio file of the response from the dispatcher
 @app.route('/api/tts/', methods=['GET'])
 def tts():
     text = request.args.get('text')
-    if text:
-        text_message = openai.generate_dispatcher_response(text, assistant_id=ASSISTANT_ID)
-        return text_message, HTTP_OK
-        # speech_file = openai.generate_speech(text, voice=VOICE, model=MODEL)
-        
-        # return "good", HTTP_OK
+    id = request.args.get('id')
+    if text and id:
+        id, text_message = openai.generate_dispatcher_response(text, assistant_id=ASSISTANT_ID, thread_id=id)
+        text_tts_path = openai.generate_speech(json.loads(text_message)["message"], VOICE, MODEL)
+        print(text_tts_path)
+        return send_file(Path(str(text_tts_path).replace("..\\\\", "", 1))), HTTP_OK
+    elif not text:
+        return 'text parameter is missing.', HTTP_BAD_REQUEST
     else:
-        return 'Text parameter is missing.', HTTP_BAD_REQUEST
+        return 'id parameter is missing.', HTTP_BAD_REQUEST
+    
+# Return the state of the call from the dispatcher
+@app.route('/api/get_call_states', methods=['GET'])
+def call_states():
+    id = request.args.get('id')
+    if id:
+        messages = openai.get_call_states(id)
+        return jsonify(messages), HTTP_OK
+    else:
+        return 'id parameter is missing.', HTTP_BAD_REQUEST
+
+# Return the call logs from the dispatcher
+@app.route('/api/get_call_log', methods=['GET'])
+def call_logs():
+    id = request.args.get('id')
+    if id:
+        messages = openai.generate_call_logs(id)
+        return jsonify(messages), HTTP_OK
+    else:
+        return 'id parameter is missing.', HTTP_BAD_REQUEST
 
 if __name__ == '__main__':
     app.run()
--- src/services/OpenAI.py
+++ src/services/OpenAI.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 from openai import OpenAI
 import uuid
+import json
 
-# Create a UUID for the speech file
-speech_file_path = Path(f"../../audio_output/openai/{uuid.uuid4()}.wav")
+# Create a UUID for the speech file. If there is not path, create one
+speech_file_path = Path(__file__).parent.parent.parent / f"audio_output\openai\{uuid.uuid4()}.mp3"
 
 class OpenAIService:
     # client: OpenAI client
@@ -22,34 +23,65 @@ class OpenAIService:
         )
         response.stream_to_file(speech_file_path)
         return speech_file_path
-    
 
-    def generate_conversation_from_array(self, conversation):
-        return conversation
+    # Generate a new chat thread for OpenAI Assistant
+    def new_chat(self):
+        thread_id = self.client.beta.threads.create().id
+        return thread_id
 
-    # Generate dispatcher response from conversation using Assistant model
+    # Generate dispatcher response from conversation using Assistant model. Also advances the thread
     def generate_dispatcher_response(self, message, assistant_id, thread_id = ""):
-        # Generate a Thread if there is no thread:
-        if thread_id == "":
-            thread_id = self.client.assistant.create_thread().id
-
-        run = self.client.beta.threads.runs.create_and_poll(
-            thread_id= thread_id,
-            assistant_id= assistant_id,
-            instructions="Please address the user as Jane Doe. The user has a premium account."
+        # Create a message in the thread
+        message = self.client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role="user",
+            content=message,
         )
 
+        # Create and poll a run for the thread
+        run = self.client.beta.threads.runs.create_and_poll(
+            thread_id=thread_id,
+            assistant_id=assistant_id,
+        )
+
+        # Retrieve the thread and messages if the run is completed
         if run.status == "completed":
             messages = self.client.beta.threads.messages.list(
-                thread_id=thread_id,
+                thread_id=thread_id,    
             )
-            print(messages)
-            return messages
+            newest_message = messages.data[0].content[0].text.value
+            print(newest_message)
+            return thread_id, newest_message
         else:
-            print(run.status)
-            return run.status
+            return thread_id, run.status
 
-    
+    def get_call_states(self, id):
+        messages = self.client.beta.threads.messages.list(
+            thread_id=id,    
+        )
+
+        message = json.loads(messages.data[0].content[0].text.value)
+        print(message)
+        return message['isFinished'], message['isPrankCall']
+
+    # Generate call logs from a thread
+    def generate_call_logs(self, id):
+        messages = self.client.beta.threads.messages.list(
+            thread_id=id,    
+        )
+
+        message_list = []
+
+        is_operator_call = len(messages.data) % 2 != 0
+        for message in messages.data:
+            is_operator_call = not is_operator_call
+            if is_operator_call:
+                message_list.insert(0, json.loads(message.content[0].text.value)["message"])
+            else:
+                message_list.insert(0, message.content[0].text.value)
+        
+        return message_list
+        
     def generate_review_response(self, conversation, model, prompt):
         return conversation
     
